3e5476668d2b6f92b16234980e7c03f647dfc01e
Pascal HAVELANGE <havelangep@hotmail.com>
2026-04-08 13:39:45 +0200

    feat: install Laravel Sanctum + API token infrastructure
    - Install laravel/sanctum v4.3
    - Publish sanctum.php config and personal_access_tokens migration
    - Add HasApiTokens trait to User model
    - Configure api guard with sanctum driver in auth.php
    - Register API routes in bootstrap/app.php with versioned structure
    - Create routes/api.php + routes/api/v1.php with /api/v1/user endpoint
    - Add feature tests for Sanctum token authentication (5 tests)
    
    Resolves #29

--- app/Models/User.php
+++ app/Models/User.php
@@ -13,11 +13,12 @@ use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Fortify\TwoFactorAuthenticatable;
+use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable implements MustVerifyEmail
 {
     /** @use HasFactory<UserFactory> */
-    use HasFactory, Notifiable, TwoFactorAuthenticatable;
+    use HasApiTokens, HasFactory, Notifiable, TwoFactorAuthenticatable;
 
     /**
      * The attributes that are mass assignable.
